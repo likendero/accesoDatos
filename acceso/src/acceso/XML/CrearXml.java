f87bc9aaa0a0ac904193563d4a1027b6e9173e14
likendero <likendero11@gmail.com>
2018-09-27 17:31:45 +0200

    funciona lo de clase

--- acceso/src/acceso/XML/CrearXml.java
+++ acceso/src/acceso/XML/CrearXml.java
@@ -24,12 +24,66 @@ public class CrearXml {
         Source source;
         Result result;
         // datos
-        int id,salario;
-        String apellido, aux;
-        // creacion flujo fichero aleatorio
+        int id,salario, posicion;
+        String apellido = new String(), departamento;
+        // necesario para parsear
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         try{
+            // Creacion de flujo de lectura
             RandomAccessFile file = new RandomAccessFile(ficheroIn, "r");
-        
+            
+            // objeto procsador o parser
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            
+            // implementacion para Dom
+            DOMImplementation implementation = builder.getDOMImplementation();
+            
+            // creacion del dcumento
+            Document document = implementation.createDocument(null, "Empleados", null);
+            document.setXmlVersion("1.0");
+            // posicion para comienzo de lectura
+            posicion = 0;
+            
+            while(posicion < file.length()){
+                // nos posicionamos
+                file.seek(posicion);
+                // obtener id empleado
+                id = file.readInt();
+                // lectura del apellido
+                apellido = file.readUTF();
+                // lectura de departamento
+                departamento = file.readUTF();
+                // lectura departamente
+                salario = file.readInt();
+                
+                if(id > 0){
+                    // creacion de un elemento que hace de raiz
+                    Element raiz = document.createElement("Empleado");
+                    // posicionamiento de la raiz
+                    document.getDocumentElement().appendChild(raiz);
+                    // añadimos sub elementos y sus valores
+                    // creacion del elemento id
+                    CrearElemento("id", Integer.toString(id), raiz, document);
+                    // creacion del elemento apellido
+                    CrearElemento("apellido", apellido, raiz, document);
+                    // creacion del elemento departamento
+                    CrearElemento("departamento", departamento, raiz, document);
+                    // creacion del elemto Salario
+                    CrearElemento("salario", Integer.toString(salario), raiz, document);
+                }
+                // cambio de la posicion
+                posicion = posicion + 122;
+            }
+            
+            // una vez creada la estructura
+            // creacion del fichero de salida
+            source = new DOMSource(document);
+            result = new StreamResult(ficheroOut);
+            
+            // objeto para realizar la transformacion
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            transformer.transform(source, result);
+            file.close();
         }catch(IOException io){
             System.out.println("error de lectura escritura");
             io.printStackTrace();
@@ -38,4 +92,22 @@ public class CrearXml {
             ex.printStackTrace();
         }
     }
+    /**
+     * metodo que introduce los valores de los elementos hoja
+     * @param dato
+     * @param valor
+     * @param raiz
+     * @param document 
+     */
+    private static void CrearElemento(String dato,String valor,Element raiz, Document document){
+        // cracion del hijo
+        Element elem = document.createElement(dato);
+        // damos valor
+        Text text = document.createTextNode(valor);
+        // pegamos el element hijo a la raiz
+        raiz.appendChild(elem);
+        // pegamos el valor
+        elem.appendChild(text);
+    }
+    
 }
