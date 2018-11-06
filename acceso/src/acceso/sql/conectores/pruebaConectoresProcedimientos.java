152681376b26c232910f49bc809a8fada10df563
likendero <likendero11@gmail.com>
2018-11-06 09:11:36 +0100

    asdojfasdf

--- acceso/src/acceso/sql/conectores/pruebaConectoresProcedimientos.java
+++ acceso/src/acceso/sql/conectores/pruebaConectoresProcedimientos.java
@@ -5,9 +5,16 @@
  */
 package acceso.sql.conectores;
 
+
+import com.mysql.cj.protocol.Resultset;
+import com.mysql.cj.x.protobuf.MysqlxDatatypes;
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Types;
 
 /**
  *
@@ -30,13 +37,28 @@ public class pruebaConectoresProcedimientos {
                     +"&useTimezone=true&serverTimezone=GMT";
             // connection
             Connection con = DriverManager.getConnection(conexion);
-            // creacion de la consulta
-            Statement sentencia = con.createStatement();
-            
+            // creacion de procedimiento
+            CallableStatement pro = con.prepareCall("{call numero_empleados(?)}");
+          
+            // datos de salida
+            pro.registerOutParameter(1, Types.INTEGER);
+            // ejecutar
+            pro.execute();
+            // capturar result set
+            //ResultSet resultSet  = pro.getResultSet();
+            // siuiente
+            //resultSet.first();
+            // mostrar
+            //System.out.println(resultSet.getInt(1));
+            int numero = pro.getInt(1);
+            System.out.println("el numero de empleados es de:" + numero);
             con.close();
             
-        }catch(){
-        
+        }catch(ClassNotFoundException cl){
+            System.out.println("clase no encontrada");
+        }catch(SQLException sq){
+            System.out.println("error sql");
+            sq.printStackTrace();
         }
     }
 }
--- filez/Scripts/procedure.sql
+++ filez/Scripts/procedure.sql
@@ -1,5 +1,36 @@
 
 delimiter //
 create procedure nombres()
-begin
-end
+begin    
+declare nombre cursor for 
+	select empleados.apellido from empresaz.empleados;
+open nombre;
+loop 
+end loop
+end//
+delimiter ;
+
+
+
+
+
+drop procedure numero_empleados;
+delimiter //
+create procedure numero_empleados(out numero int)
+begin    
+select count(*) into numero from empleados ;
+
+ 
+
+end//
+delimiter ;
+
+
+
+set @a as int;
+
+
+
+
+
+
